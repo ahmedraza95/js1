d5a2e8e78e97f86f4f791af4126d3b070cdcb630
ahmedraza95 <ahmedhome6789@gmail.com>
2024-03-08 01:01:55 +0500

    first

---
+++ app.js
@@ -0,0 +1,6 @@
+let secondValue = prompt("type first number which you want to add");
+let firstValue = prompt("type second number which you want to add");
+// let ans = secondValue add firstValue 
+let final = (+secondValue + +firstValue);
+let result = alert("your sum is" + "" + final);
+console.log(final)
